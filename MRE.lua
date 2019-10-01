f85a830406e194f0e200188b8fb95efb99d63596
Simon Hirsig <simon.hirsig@gmail.com>
2019-10-01 20:56:13 +0200

    classic compatibility changes

--- MRE.lua
+++ MRE.lua
@@ -1,12 +1,10 @@
 local SCALE = 1
 local FONT = [[Fonts\FRIZQT__.ttf]]
 local COLOR = {
-	MANA = {0, 0, 1},
-	RAGE = {1, 0, 0},
-	ENERGY = {1, 1, 0},
+	[0] = {0, 0, 1},
+	[1] = {1, 0, 0},
+	[3] = {1, 1, 0},
 }
-
-MRE_TYPE = 'MANA'
 local frame = CreateFrame('Frame', nil, UIParent)
 frame:SetWidth(1)
 frame:SetHeight(32 * SCALE)
@@ -20,24 +18,21 @@ text:SetTextHeight(32 * SCALE)
 frame:SetMovable(true)
 frame:SetClampedToScreen(true)
 frame:RegisterForDrag('LeftButton')
-frame:SetScript('OnUpdate', function() this:EnableMouse(IsAltKeyDown() or this.dragging) end)
-frame:SetScript('OnDragStart', function() this.dragging = true this:StartMoving() end)
-frame:SetScript('OnDragStop', function() this.dragging = false this:StopMovingOrSizing() MRE_POSITION = {frame:GetCenter()} end)
-frame:SetScript('OnEvent', function(_, event, arg1)
-	if event ~= 'PLAYER_LOGIN' then
-		if arg1 ~= 'player' then return end
-		MRE_TYPE = gsub(gsub(event, 'UNIT_', ''), 'MAX', '')
-	end
-	local fraction = UnitMana('player') / UnitManaMax('player')
-	for i, component in pairs(COLOR[MRE_TYPE]) do
+frame:SetScript('OnUpdate', function(self) self:EnableMouse(IsAltKeyDown() or self.dragging) end)
+frame:SetScript('OnDragStart', function(self) self.dragging = true self:StartMoving() end)
+frame:SetScript('OnDragStop', function(self) self.dragging = false self:StopMovingOrSizing() MRE_POSITION = {frame:GetCenter()} end)
+frame:SetScript('OnEvent', function(_, event, arg1, arg2)
+	if event == 'UNIT_POWER_UPDATE' and arg1 ~= 'player' then return end
+	local fraction = UnitPower('player') / UnitPowerMax('player')
+	for i, component in pairs(COLOR[UnitPowerType'player']) do
 		text[({'r','g','b'})[i]] = component * fraction + (1 - fraction)
 	end
-	text:SetText(UnitMana('player'))
+	text:SetText(UnitPower('player'))
 	frame:SetWidth(text:GetStringWidth())
 	frame:ClearAllPoints()
 	frame:SetPoint('CENTER', UIParent, 'BOTTOMLEFT', unpack(MRE_POSITION or {frame:GetCenter()}))
 	text:SetTextColor(text.r, text.g, text.b)
 end)
-for _, event in pairs{'PLAYER_LOGIN', 'UNIT_MANA', 'UNIT_MAXMANA', 'UNIT_RAGE', 'UNIT_MAXRAGE', 'UNIT_ENERGY', 'UNIT_MAXENERGY'} do
+for _, event in pairs{'PLAYER_LOGIN', 'UNIT_POWER_UPDATE'} do
 	frame:RegisterEvent(event)
 end
